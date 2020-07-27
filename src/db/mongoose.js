9b3f8a4653d568837c468340b7834197497157f3
Avshalom <avshalom.tam@gmail.com>
2020-07-27 17:01:38 +0300

    Initial commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+
+// Run to connect to mongoDB
+// C:\Users\avsha\mongodb\bin\mongod.exe --dbpath=C:/Users/avsha/mongodb-data
